8101b9a4213f7cc61068600d00130192c5ecab8c
MateuszKMechaniktg <mateusz.kubica@uczen.mechaniktg.pl>
2026-01-09 11:40:20 +0100

    binding

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -12,49 +12,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.koszykowka.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView text;
-    private Button button1, button2, button3;
+    //private TextView text;
+    //private Button button1, button2, button3;
     private int punkty;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //setContentView(R.layout.activity_main);
 
-        text = findViewById(R.id.textView);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
+        //text = findViewById(R.id.textView);
+        //button1 = findViewById(R.id.button1);
+        //button2 = findViewById(R.id.button2);
+        //button3 = findViewById(R.id.button3);
         if(savedInstanceState != null){
             punkty = savedInstanceState.getInt("PUNKTY",0);
         }
-       text.setText(String.valueOf(punkty));
+       binding.textView.setText(String.valueOf(punkty));
 
-        button1.setOnClickListener(
+        binding.button1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         punkty++;
-                        text.setText(String.valueOf(punkty));
+                        binding.textView.setText(String.valueOf(punkty));
                     }
                 }
         );
-        button2.setOnClickListener(
+        binding.button2.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         punkty+=2;
-                        text.setText(String.valueOf(punkty));
+                        binding.textView.setText(String.valueOf(punkty));
                     }
                 }
         );
-        button3.setOnClickListener(
+        binding.button3.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         punkty+=3;
-                        text.setText(String.valueOf(punkty));
+                        binding.textView.setText(String.valueOf(punkty));
                     }
                 }
         );

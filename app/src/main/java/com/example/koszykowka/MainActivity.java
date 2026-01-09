333c16a1f64ee57c272e1092c3ac475d3ecb8f31
MateuszKMechaniktg <mateusz.kubica@uczen.mechaniktg.pl>
2026-01-09 11:34:13 +0100

    zapisywanie2

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -21,19 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         text = findViewById(R.id.textView);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         if(savedInstanceState != null){
-            punkty = savedInstanceState.getInt("PUNKTY");
+            punkty = savedInstanceState.getInt("PUNKTY",0);
         }
-        text.setText(String.valueOf(punkty));
+       text.setText(String.valueOf(punkty));
 
         button1.setOnClickListener(
                 new View.OnClickListener() {
@@ -63,9 +59,10 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("PUNKTY", punkty);
+        outState.putInt("PUNKTY",punkty);
     }
 }
